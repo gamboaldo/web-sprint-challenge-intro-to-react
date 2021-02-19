0cec3eb6d05eaa51368630901d6ffd5b13f8270a
ALDO <aldo@aldos-mbp.attlocal.net>
2021-02-19 09:16:36 -0800

    MVP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { BASE_URL } from "./constants/base_url";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  console.log(axios.get("https://swapi.py4e.com/api/people/"));
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([
+    {
+      name: "ALDO MATA",
+    },
+    {
+      name: "is the greatest",
+    },
+  ]);
+
+  useEffect(() => {
+    const getCharacters = () => {
+      axios
+        .get(`${BASE_URL}people/`)
+        .then((res) => {
+          console.log(res.data);
+          setCharacters(res.data.results);
+        })
+        .catch((err) => {
+          console.log(err);
+          debugger;
+        });
+    };
+    getCharacters();
+  }, []);
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">STAR WARS CHARACTERS!</h1>
+      {characters.map((character) => {
+        return (
+          <Character
+            key={character.name}
+            name={character.name}
+            gender={character.gender}
+            birthYear={character.birth_year}
+            height={character.height}
+            mass={character.mass}
+          ></Character>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,60 @@
 // Write your Character component here
+import React, { useState } from "react";
+import styled from "styled-components";
+
+export default function Character({ name, gender, birthYear, height, mass }) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <StyledCharacter expanded={expanded} onClick={() => setExpanded(!expanded)}>
+      <h2>{name}</h2>
+      <span className="arrow-down">
+        {expanded
+          ? `${String.fromCharCode(8963)}`
+          : `${String.fromCharCode(8964)}`}
+      </span>
+      <p>Gender: {gender}</p>
+      <p>Birth Year: {birthYear}</p>
+      <p>Height: {height}</p>
+      <p>Mass: {mass}</p>
+    </StyledCharacter>
+  );
+}
+
+const StyledCharacter = styled.div`
+  /* External */
+  cursor: pointer;
+  margin: 0 auto;
+  width: 80%;
+  max-width: 80rem;
+  /* Internal */
+  position: relative;
+  height: ${(props) => (props.expanded ? `22rem` : `5rem`)};
+  overflow: hidden;
+  transition: all 0.7s ease;
+
+  /* Colors and Styles */
+  background-color: white;
+  border-bottom: 1px solid #bbb;
+  opacity: 0.7;
+  /*  Element Styles  */
+  h2 {
+    font-size: 2rem;
+    line-height: 5rem;
+    margin: 0;
+    padding-left: 2rem;
+    text-align: left;
+  }
+  span.arrow-down {
+    font-size: 2rem;
+    position: absolute;
+    top: ${(props) => (props.expanded ? "1.5rem" : "0.7rem")};
+    right: 2%;
+  }
+  p {
+    font-size: 1.5rem;
+    line-height: 2rem;
+    padding-left: 2.2rem;
+    text-align: left;
+  }
+`;
